24bb228093efb65485c31aa7294e175ebec59a6f
obulka <obulka@hotmail.com>
2023-06-16 00:34:43 -0700

    made primitive generic

--- damascus_core/src/geometry/geometry.rs
+++ damascus_core/src/geometry/geometry.rs
@@ -10,27 +10,48 @@ pub struct Transform {
     //skew: Vec3,
 }
 
+pub trait Primitive {
+    fn id(self) -> u32;
+    fn transform(self) -> Transform;
+    fn material(self) -> Material;
+    fn modifiers(self) -> u32;
+    fn blend_strength(self) -> f32;
+    fn num_children(self) -> u32;
+}
+
 #[derive(Default)]
-pub struct Primitive {
+pub struct Sphere {
+    pub id: u32,
     pub transform: Transform,
     pub material: Material,
     pub modifiers: u32,
     pub blend_strength: f32,
     pub num_children: u32,
-}
-
-pub trait BasePrimitive {
-    fn primitive(self) -> Primitive;
-}
-
-#[derive(Default)]
-pub struct Sphere {
     pub radius: f32,
-    primitive: Primitive,
 }
 
-impl BasePrimitive for Sphere {
-    fn primitive(self) -> Primitive {
-        self.primitive
+impl Primitive for Sphere {
+    fn id(self) -> u32 {
+        self.id
+    }
+
+    fn transform(self) -> Transform {
+        self.transform
+    }
+
+    fn material(self) -> Material {
+        self.material
+    }
+
+    fn modifiers(self) -> u32 {
+        self.modifiers
+    }
+
+    fn blend_strength(self) -> f32 {
+        self.blend_strength
+    }
+
+    fn num_children(self) -> u32 {
+        self.num_children
     }
 }

4a4191c7f4a7d79130294f7e1a47354b1f37bff4
obulka <obulka@hotmail.com>
2026-04-12 16:02:55 -0700

    removed commented code

--- damascus_ui/src/widgets/toolbar/mod.rs
+++ damascus_ui/src/widgets/toolbar/mod.rs
@@ -194,16 +194,10 @@ impl Widget<ToolbarMessage> for Toolbar {
         _window_id: iced::window::Id,
         preferences: &'a style::Preferences,
     ) -> iced::Element<'a, ToolbarMessage> {
-        // let mut modal =
-        //     egui_modal::Modal::new(egui_context, "dialog_modal").with_style(&egui_modal::ModalStyle {
-        //         ..Default::default()
-        //     });
-        // modal.show_dialog();
-
         iced::widget::row(Menus::iter().map(|menu_option| {
             menu::DropdownMenu::new(
                 menu_option.menu_options(),
-                menu_option.to_string(),
+                menu_option.variant(),
                 |option| -> ToolbarMessage { ToolbarMessage::from_str(&option).into() },
             )
             .style(|theme: &iced::Theme, status| -> menu::Style {
@@ -220,112 +214,5 @@ impl Widget<ToolbarMessage> for Toolbar {
         }))
         .spacing(3)
         .into()
-
-        // egui::TopBottomPanel::top("toolbar").show(egui_context, |ui| {
-        //     egui::MenuBar::new().ui(ui, |ui| {
-        //         // File menu
-        //         let mut new_requested: bool = egui_context.memory(|memory| memory.focused().is_none())
-        //             && egui_context.input(|input| {
-        //                 input.key_pressed(egui::Key::N)
-        //                     && input.modifiers.matches_logically(egui::Modifiers::CTRL)
-        //             });
-        //         let mut load_requested: bool = egui_context.memory(|memory| memory.focused().is_none())
-        //             && egui_context.input(|input| {
-        //                 input.key_pressed(egui::Key::L)
-        //                     && input.modifiers.matches_logically(egui::Modifiers::CTRL)
-        //             });
-        //         let mut save_requested: bool = egui_context.memory(|memory| memory.focused().is_none())
-        //             && egui_context.input(|input| {
-        //                 input.key_pressed(egui::Key::S)
-        //                     && input.modifiers.matches_exact(egui::Modifiers::CTRL)
-        //             });
-        //         let mut save_as_requested: bool = egui_context
-        //             .memory(|memory| memory.focused().is_none())
-        //             && egui_context.input(|input| {
-        //                 input.key_pressed(egui::Key::S)
-        //                     && input
-        //                         .modifiers
-        //                         .matches_exact(egui::Modifiers::CTRL | egui::Modifiers::SHIFT)
-        //             });
-
-        //         let success_dialog: bool = !save_requested;
-
-        //         ui.menu_button("File", |ui| {
-        //             new_requested |= ui
-        //                 .add(egui::Button::new("new").shortcut_text("Ctrl+N"))
-        //                 .clicked();
-        //             load_requested |= ui
-        //                 .add(egui::Button::new("load").shortcut_text("Ctrl+L"))
-        //                 .clicked();
-        //             save_requested |= ui
-        //                 .add(egui::Button::new("save").shortcut_text("Ctrl+S"))
-        //                 .clicked();
-        //             save_as_requested |= ui
-        //                 .add(egui::Button::new("save as").shortcut_text("Ctrl+Shift+S"))
-        //                 .clicked();
-
-        //             if load_requested || save_requested || save_as_requested {
-        //                 ui.close();
-        //             }
-        //         });
-
-        //         save_requested |= new_requested && context.dirty(node_graph);
-
-        //         let mut saved: bool = false;
-        //         if load_requested {
-        //             let mut file_dialog = rfd::FileDialog::new()
-        //                 .set_title("load from file")
-        //                 .add_filter("damascus", &["dam"]);
-        //             if let Some(file_path) = &context.working_file {
-        //                 if let Some(directory) = std::path::Path::new(file_path).parent() {
-        //                     file_dialog = file_dialog.set_directory(directory);
-        //                 }
-        //                 file_dialog = file_dialog.set_file_name(file_path);
-        //             }
-        //             if let Some(path) = file_dialog.pick_file() {
-        //                 let file_path: String = path.display().to_string();
-        //                 load(&file_path, node_graph, &modal);
-        //                 context.update(file_path.to_string(), node_graph);
-        //             }
-        //         } else if save_requested {
-        //             if let Some(file_path) = &context.working_file {
-        //                 save(file_path, node_graph, &modal, success_dialog);
-        //                 context.update(file_path.to_string(), node_graph);
-        //                 saved = true;
-        //             }
-        //         }
-        //         if save_requested && !saved || save_as_requested {
-        //             let mut file_dialog = rfd::FileDialog::new()
-        //                 .set_title("save to file")
-        //                 .add_filter("damascus", &["dam"]);
-        //             if let Some(file_path) = &context.working_file {
-        //                 if let Some(directory) = std::path::Path::new(file_path).parent() {
-        //                     file_dialog = file_dialog.set_directory(directory);
-        //                 }
-        //                 file_dialog = file_dialog.set_file_name(file_path);
-        //             }
-        //             if let Some(path) = file_dialog.save_file() {
-        //                 let file_path: String = path.display().to_string();
-        //                 save(&file_path, node_graph, &modal, true);
-        //                 context.update(file_path, node_graph);
-        //             }
-        //         }
-        //         if new_requested {
-        //             node_graph.clear();
-        //             *context = Context::default();
-        //         }
-
-        //         // Cache menu
-        //         ui.menu_button("Cache", |ui| {
-        //             ui.horizontal(|ui| {
-        //                 if ui.button("clear node cache").clicked() {
-        //                     node_graph.output_cache.clear();
-        //                 }
-        //             });
-        //         });
-        //     });
-        // });
-
-        // Vec::<NodeGraphResponse>::new()
     }
 }
